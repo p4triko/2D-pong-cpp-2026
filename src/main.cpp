c7907d273fb3f89b1266e6ddfe4daf646684df4e
Patrick <168976850+p4triko@users.noreply.github.com>
2026-04-26 10:13:46 +0300

    Basic üles alla liikumine

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,40 @@
 
 int main()
 {
-	sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "SFML works!" );
-	sf::CircleShape shape( 100.f );
-	shape.setFillColor( sf::Color::Green );
+	sf::RenderWindow window(sf::VideoMode({1000, 720}), "2D Pong");
+	window.setFramerateLimit(60);
 
-	while ( window.isOpen() )
+	sf::RectangleShape player({30.f, 120.f});
+	player.setPosition({10.f, static_cast<float>(window.getSize().y / 2.f)});
+	player.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
 	{
-		while ( const std::optional event = window.pollEvent() )
+		while (const std::optional event = window.pollEvent())
 		{
-			if ( event->is<sf::Event::Closed>() )
+			if (event->is<sf::Event::Closed>())
+			{
 				window.close();
+			}
+			else if (const auto *keyPressed = event->getIf<sf::Event::KeyPressed>())
+			{
+				if (keyPressed->scancode == sf::Keyboard::Scancode::Escape)
+					window.close();
+			}
+
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+			{
+				player.move({0.f, -1.f});
+			}
+
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+			{
+				player.move({0.f, 1.f});
+			}
 		}
 
 		window.clear();
-		window.draw( shape );
+		window.draw(player);
 		window.display();
 	}
 }
